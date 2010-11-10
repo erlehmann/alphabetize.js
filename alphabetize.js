ec98fbb350f47bb77e1268afc9c9dbdd6393f692
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-10 12:59:11 +0100

    + image luma sorting

--- alphabetize.js
+++ alphabetize.js
@@ -31,7 +31,76 @@ var xPathResult = document.evaluate(
     null
 );
 
-for(var i = 0, l = xPathResult.snapshotLength; i < l + 1; i++) {
+for(var i = 0, l = xPathResult.snapshotLength; i < l; i++) {
     var textNode = xPathResult.snapshotItem(i);
-    textNode.data = alphabetize(textNode.data);
+    if (textNode != null) {
+        textNode.data = alphabetize(textNode.data);
+    }
+};
+
+/* now images will be “sorted” */
+
+function luma(pixel) {
+    return ((0.299 * pixel[0]) + (0.114 * pixel[1]) + (0.587 * pixel[2])) * (pixel[3] / 256)
+}
+
+function isort(x, y) {
+    a = luma(x);
+    b = luma(y);
+    if (a > b) { return 1; }
+    if (a < b) { return -1; }
+    return 0;
+}
+
+function lumanize(image) {
+    var canvas = document.createElement("canvas");
+    var canvasContext = canvas.getContext("2d");
+
+    var width = image.width;
+    var height = image.height;
+    canvas.width = width;
+    canvas.height = height;
+    canvasContext.drawImage(image, 0, 0);
+
+    var imageData = canvasContext.getImageData(0, 0, width, height);
+    var imageArray = new Array(0);
+
+    /* copy imageData to array */
+    for (y = 0; y < height; y++) {
+        inpos = y * width * 4;
+        for (x = 0; x < width; x++) {
+            var pixel = new Array(4);
+            /* rgba */
+            pixel[0] = imageData.data[inpos++];
+            pixel[1] = imageData.data[inpos++];
+            pixel[2] = imageData.data[inpos++];
+            pixel[3] = imageData.data[inpos++];
+
+            imageArray.push(pixel);
+        }
+    };
+
+    imageArray = imageArray.sort(isort);
+
+    /* copy array to imageData */
+    for (y = 0; y < height; y++) {
+        outpos = y * width * 4;
+        for (x = 0; x < width; x++) {
+            pixel = imageArray.pop();
+            /* rgba */
+            imageData.data[outpos++] = pixel[0];
+            imageData.data[outpos++] = pixel[1];
+            imageData.data[outpos++] = pixel[2];
+            imageData.data[outpos++] = pixel[3];
+        }
+    };
+
+    canvasContext.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
+    return canvas.toDataURL();
+}
+
+var images = document.getElementsByTagName("img");
+for (i = 0; i < images.length; i++) {
+    var image = images[i];
+    image.src = lumanize(image);
 }
