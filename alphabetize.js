c3d7004fe08adfc9ebb2de31b8bfaac40dc6034a
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-11 02:38:39 +0100

    * fix for alpha component in luminosity formula

--- alphabetize.js
+++ alphabetize.js
@@ -41,7 +41,7 @@ for(var i = 0, l = xPathResult.snapshotLength; i < l; i++) {
 /* now images will be “sorted” */
 
 function luminance(pixel) {
-    return ((0.2126 * pixel[0]) + (0.7152 * pixel[1]) + (0.0722 * pixel[2])) * (pixel[3] / 255);
+    return ((0.2126 * pixel[0]) + (0.7152 * pixel[1]) + (0.0722 * pixel[2])) * (1 / pixel[3]);
 }
 
 function isort(x, y) {
